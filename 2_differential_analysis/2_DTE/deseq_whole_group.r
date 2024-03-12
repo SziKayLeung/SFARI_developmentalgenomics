f3e9d5ed70631cafd8f317cae59d2748b2b5fe88
sl693 <sl693@exeter.ac.uk>
2024-03-12 16:07:47 +0000

    Cleaned differential expression analysis

--- 2_differential_analysis/2_DTE/deseq_whole_group.r
+++ 2_differential_analysis/2_DTE/deseq_whole_group.r
@@ -1,141 +1,164 @@
-##Szi Kay run_DESSeq2.R script
+#!/usr/bin/env Rscript
+## ----------Script-----------------
+##
+## Author: R.Bamford, K.Chundru, S.Leung (S.K.Leung@exeter.ac.uk)
+## output a list of transcripts that are documented in the TALON gtf but not in the abundance file
+## sanity check for TALON pipeline
+## Input:
+##  --gtf = TALON gtf generated from talon_create_GTF
+##  --counts = TALON abundance file
+##  --output = output path and name of file
+## --------------------------------
 
+## ---------- packages -----------------
 
-library(dplyr) #needed for %>% command
+library(dplyr)
 library(DESeq2)
 library(ggrepel)
 library(data.table)
-source("/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARIdevelopmentalgenomics/12_deseq2/run_DESeq2.R")
-
-#Phenotype file
-phenotype=read.csv("/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/RBFetal/00_metadata/WholeTargetedphenotype_fixedsex.csv")
-phenotype=phenotype[which(startsWith(phenotype$sample, 'Whole')),]
-#phenotype=phenotype[which((phenotype$group=='Prenatal'&phenotype$age>30)==F & (phenotype$group=='Postnatal'&phenotype$age<2)==F),]
-#phenotype[which(phenotype$group=='Prenatal'),]
-#phenotype[which(phenotype$age < 10),]
-#phenotype=read.csv("/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARIdevelopmentalgenomics/12_deseq2/phenotype.csv")
-#phenotype=read.csv("/gpfs/mrc0/projects/Research_Project-MRC148213/Rosie/SortedNuclei/RNA/P0075_20230216_10916/adult_sorted_nuclei/20230216_1558_1G_PAK65840_66586f9d/combined/phenotype.csv")
+library(optparse)
 
 
-#Expression file (demux csv - make sure first column is isoform; all samples are there and that the sample names match up with those in the phenotype file)
-expression=fread("/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARIdevelopmentalgenomics/12_deseq2/WholeTargeted_demux.csv",data.table=F)
+## ---------- arguments -----------------
 
-#selecting the samples (whole in this case)
-expression = expression[,c(1,which(colnames(expression)%in%phenotype$sample))]
+option_list <- list( 
+  make_option(c("-p", "--phenotype"), type="character", default=NULL, 
+              help="Phenotype file", metavar="character"),
+  make_option(c("-e", "--expession"), type="character", default=NULL, 
+              help="Expression file"),
+  make_option(c("-t", "--threshold"), type="numeric", default=10L, 
+              help="read threshold for DESeq"),
+  make_option(c("-d", "--design"), type="character", default=NULL, 
+              help="<development><sex>"),
+  make_option(c("-l", "--level"), type="character", default="transcript", 
+              help="<transcript><gene>"),
+  make_option(c("-i", "--dataset"), type="character", default="whole", 
+              help="<targeted><whole>"),
+  make_option(c("-o", "--output"), type="character", default=NULL, 
+              help="output file and path of missing transcripts in abundance file", metavar="character")
+)
 
-#adding 1 to each cell
-#expression <- expression %>% mutate(across(where(is.numeric), ~ .x +1))
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
 
-#Filter expression file for targeted genes (but not filtered on abundance)
-#input.class.files="/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/RBFetal/1_SQANTI3/SQANTI3_collapse_options_RulesFilter_result_classification.targetgenes_counts.txt"
 
-input.class.files="/gpfs/mrc0/projects/Research_Project-MRC148213/Rosie/WholeTargeted/SQANTI/unfiltered/WholeTargeted_cleaned_aligned_merged_collapsed_qced_RulesFilter_classification.txt"
+## ---------- Input files -----------------
 
-class.files=fread(input.class.files,sep="\t",header=T,data.table=F)
-#print(head(class.files))
-class.files <- class.files[which(class.files$filter_result=="Isoform"),]
+# phenotype
+message("Read in: ", opt$phenotype)
+phenotype <- read.csv(opt$phenotype) 
+#print(phenotype)
 
-expression <-  right_join(expression,class.files[,c("isoform","associated_gene")], by = "isoform")# %>% dplyr::select(-associated_gene)
+# expression (demux csv - make sure first column is isoform; all samples are there and that the sample names match up with those in the phenotype file)
+message("Read in: ", opt$expression)
+expression <- fread(opt$expression, data.table=F)
+
+# classification file
+class.files <- fread(opt$classfile, sep="\t", header=T, data.table=F)
+row.names(class.files) <- class.files$isoform
+
+
+## ---------- data wrangle -----------------
+
+## --- class.files --- 
+# column (filter_result) present in classification file, make sure to only select true isoforms 
+# otherwise assumed input classification file has been filtered prior 
+if(exists("filter_result", where = class.files) == TRUE){
+  class.files <- class.files[which(class.files$filter_result=="Isoform"),]
+}
+class.files <- class.files[,c("isoform","associated_gene","associated_transcript","structural_category","subcategory","exons")]
+
+## --- expression --- 
+# subset samples based on the phenotype file
+expression <- expression[,c(1,which(colnames(expression)%in%phenotype$sample))]
+
+# subset expression to only the transcripts in classification file
+expression <-  right_join(expression,class.files[,c("isoform","associated_gene")], by = "isoform")
+
+# filter rows with 0 reads
 expression <- expression[which(rowSums(expression[,2:(ncol(expression)-1)])>0),]
-#tgenes<-read.table('/gpfs/mrc0/projects/Research_Project-MRC148213/vc362/fetalBrain/genes.txt', stringsAsFactors=F)
-#expression <- expression[which(expression$associated_gene%in%tgenes$V1),]
-expression <- expression %>% dplyr::select(-associated_gene)
 
-
-#fwrite(expression,file="wholetargetedfiltered_demux.csv", quote=F)
-
-#nThread=16 helps with big files
-#print(head(expression))
-#phenotype$sex <- as.factor(phenotype$sex)
-phenotype$group <- as.factor(phenotype$group)
-#phenotype$group <- factor(phenotype$group, levels=c("Early", "Mid...)
-
-#phenotype$group <- relevel(phenotype$group,"NeuN")
-#phenotype$sex <- relevel(phenotype$sex,"M")
-
-phenotype$group <- relevel(phenotype$group,"Postnatal")
-#phenotype$sex <- relevel(phenotype$sex,"M")
-
-str(phenotype$group)
-#str(phenotype$sex)
-
-col_match <- intersect(phenotype$sample, colnames(expression))
-cat("Number of samples:", length(col_match),"\n")
-
-
-#expression <- expression %>% tibble::column_to_rownames("isoform")
-rownames(expression)<-expression$isoform
-phenotype <- phenotype %>% filter(sample %in% col_match) 
-expression <- expression %>% dplyr::select(phenotype$sample)
-
-if(nrow(phenotype) != ncol(expression)){
-	print("Mismatched number of samples in phenotype and expression file")
+# remove associated gene column
+if(opt$level == "gene"){
+  expression <- as.data.frame(expression %>%
+                                group_by(associated_gene) %>%
+                                select(-isoform) %>% summarize_all(.funs = list(sum)))
+  rownames(expression)<- expression$associated_gene
+  expression <- expression %>% dplyr::select(-associated_gene)
+  
+}else{
+  rownames(expression) <- expression$isoform
+  expression <- expression %>% dplyr::select(-associated_gene, isoform)
 }
 
-cat("Expression file:\n")
+## --- phenotype --- 
+# factor group level 
+if(opt$design == "development"){
+  phenotype$group <- as.factor(phenotype$group)
+  phenotype$group <- relevel(phenotype$group,"Postnatal")
+  str(phenotype$group)
+}else{
+  phenotype$sex <- as.factor(phenotype$sex)
+  phenotype$sex <- relevel(phenotype$sex,"M")
+  str(phenotype$sex)
+}
 
 
-dds <- DESeqDataSetFromMatrix(countData = as.matrix(expression), colData = phenotype, design = ~ group)
+## matching samples
+if(nrow(phenotype) != ncol(expression)){
+	message("Mismatched number of samples in phenotype and expression file")
+  message("Using common samples in both files")
+  col_match <- intersect(phenotype$sample, colnames(expression))
+  cat("Number of samples:", length(col_match),"\n")
+  phenotype <- phenotype %>% filter(sample %in% col_match) 
+  expression <- expression %>% dplyr::select(phenotype$sample)
+}
 
+
+## ---------- run DESeq -----------------
+
+if(opt$design == "development"){
+  message("Design: ~ group")
+  dds <- DESeqDataSetFromMatrix(countData = as.matrix(expression), colData = phenotype, design = ~ group)
+}else{
+  message("Design: ~ sex")
+  dds <- DESeqDataSetFromMatrix(countData = as.matrix(expression), colData = phenotype, design = ~ sex)
+}
 dds <- estimateSizeFactors(dds)
-
 norm <- counts(dds, normalized=TRUE) %>% reshape2::melt() %>% `colnames<-`(c("isoform", "sample", "normalised_counts"))
-phenotype=phenotype[which((phenotype$group=='Prenatal'&phenotype$age>30)==F & (phenotype$group=='Postnatal'&phenotype$age<2)==F),]
-
-input.class.files="/gpfs/mrc0/projects/Research_Project-MRC148213/Rosie/WholeTargeted/SQANTI/WholeTargeted_cleaned_aligned_merged_collapsed_qced_RulesFilter_2reads2samples_classification.txt"
-
-class.files=read.table(input.class.files,sep="\t",as.is=T,header=T)
-
-#dds<-dds[which(rownames(dds)%in%class.files$isoform),which(dds$sample%in%phenotype$sample)]
-dds<-dds[,which(dds$sample%in%phenotype$sample)]
-
-threshold=10
 
 cat("Number of isoforms before filtering:", nrow(dds),"\n")
-cat("Filtering isoform on count threshold:", threshold,"\n")
-dds <- dds[rowSums(counts(dds)) > threshold, ] 
+cat("Filtering isoform on count threshold:", opt$threshold,"\n")
+dds <- dds[rowSums(counts(dds)) > opt$threshold, ] 
 cat("Number of isoforms after filtering:", nrow(dds),"\n")
 
 cat("Running Wald test\n")
 dds_Wald <- DESeq(dds, test="Wald")
-res_Wald <- as.data.frame(results(dds_Wald)) %>% tibble::rownames_to_column("isoform") %>% arrange(padj) 
+if(opt$level == "gene"){
+  res_Wald <- as.data.frame(results(dds_Wald)) %>% tibble::rownames_to_column("associated_gene") %>% arrange(padj) 
+}else{
+  res_Wald <- as.data.frame(results(dds_Wald)) %>% tibble::rownames_to_column("isoform") %>% arrange(padj) 
+}
 
 deseq_output <- list(dds_Wald, res_Wald, norm)
-
 names(deseq_output) <- c("dds_Wald", "res_Wald","norm_counts")
 
-fwrite(deseq_output$res_Wald, file="output_whole_group_removinglateprenatal_TEST.csv")
-fwrite(deseq_output$res_Wald[which(deseq_output$res_Wald$padj<0.05),], file="filtered_output_whole_group_removinglateprenatal_TEST.csv")
+## ---------- output -----------------
 
-fwrite(deseq_output$norm,file="output_norm_whole_group_removinglateprenatal_TEST.csv")
-fwrite(deseq_output$norm[which(deseq_output$norm$isoform%in%deseq_output$res_Wald[which(deseq_output$res_Wald$padj<0.05),]$isoform),],file="filtered_output_norm_whole_group_removinglateprenatal_TEST.csv")
+# wald results and annotate with classification file
+if(opt$level == "gene"){
+  deseq_output$norm <- deseq_output$norm %>% dplyr::rename("associated_gene" = "isoform")
+  deseq_output$anno_res <- deseq_output$res_Wald
+  deseq_output$anno_norm <- deseq_output$norm
+}else{
+  deseq_output$anno_res <-  right_join(deseq_output$res_Wald,class.files, by = "isoform")
+  deseq_output$anno_norm <- right_join(deseq_output$norm,class.files, by = "isoform")
+}
 
-##Targeted
-#input.class.files="/gpfs/mrc0/projects/Research_Project-MRC148213/Rosie/WholeTargeted/SQANTI/WholeTargeted_cleaned_aligned_merged_collapsed_qced_RulesFilter_2reads2samples_classification.txt"
+fwrite(deseq_output$anno_res, file=paste0(opt$directory, "/DESeq2_", opt$dataset,"_",opt$level,"_",opt$design,"_resAll.csv"))
+fwrite(deseq_output$anno_res[which(deseq_output$anno_res$padj<0.05),], file=paste0(opt$directory, "/DESeq2_", opt$dataset,"_", opt$level, "_", opt$design,"_resSig.csv"))
 
-#class.files=read.table(input.class.files,sep="\t",as.is=T,header=T)
-##print(head(class.files))
-##annooutput = anno_DESeq2(deseq_output,class.files,phenotype,controlname="M",level="associated_transcript",sig=0.05)
-
-
-##write.csv(annooutput$anno_res,file="annotated_output_targeted.csv")
-
-##phenotype$group <- as.factor(phenotype$group)
-##phenotype$group <- relevel(phenotype$group,"NeuN")
-
-deseq_output$anno_res <-  right_join(deseq_output$res_Wald,class.files[,c("isoform","associated_gene","associated_transcript","structural_category","subcategory")], by = "isoform")
-
-fwrite(deseq_output$anno_res,file="anno_whole_group_removinglateprenatal_TEST.csv", quote=F)
-
-##deseq_output[[3]] <- merge(deseq_output[[3]],class.files[,c("isoform","associated_gene","associated_transcript")], by = "isoform", all.x = T)
-##deseq_output[[3]] <- merge(deseq_output[[3]],phenotype, by = "sample", all = T)
-
-##write.csv(deseq_output[[3]],file="anno_output.csv")
-
-SFARI=fread("/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARIdevelopmentalgenomics/12_deseq2/SFARI-Gene_genes_01-23-2023release_03-24-2023export.csv",data.table=F)
-
-##class.files=read.table(SFARI,sep="\t",as.is=T,header=T)
-
-SFARI_hits <-  right_join(deseq_output$anno_res,SFARI[,c("gene-symbol","associated_gene")], by = "associated_gene")
-
-fwrite(SFARI_hits,file="SFARI_hits_whole_group_removinglateprenatal_TEST.csv", quote=F)
+# normalisation
+fwrite(deseq_output$anno_norm,file=paste0(opt$directory, "/DESeq2_", opt$dataset,"_", opt$design,"_normAll.csv"))
+normSig <- deseq_output$anno_norm[which(deseq_output$anno_norm$isoform%in%deseq_output$res_Wald[which(deseq_output$res_Wald$padj<0.05),]$isoform),]
+fwrite(normSig,file=paste0(opt$directory, "/DESeq2_", opt$dataset,"_", opt$design,"_normSig.csv"))
--- 2_differential_analysis/3_DIU/2_wholedataset_DIU.sh
+++ 2_differential_analysis/3_DIU/2_wholedataset_DIU.sh
@@ -2,20 +2,19 @@
 #SBATCH --export=ALL # export all environment variables to the batch job
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq # submit to the parallel queue
-#SBATCH --time=10:00:00 # maximum walltime for the job
+#SBATCH --time=30:00:00 # maximum walltime for the job
 #SBATCH -A Research_Project-MRC148213 # research project to submit under
 #SBATCH --nodes=1 # specify number of nodes
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion
 #SBATCH --mail-user=sl693@exeter.ac.uk # email address
-#SBATCH --output=2_wholedataset_DIU-%A_%a.o
-#SBATCH --error=2_wholedataset_DIU-%A_%a.e
-#SBATCH --array=0-1
+#SBATCH --array=0-19 # 20 files split across 17,000 protein coding genes
+#SBATCH --output=Log/2_wholedataset_proteincoding_DIU-%A_%a.o
+#SBATCH --error=Log/2_wholedataset_proteincoding_DIU-%A_%a.e
 
 
-# 13/10/2023: run DIU in targeted group dataset
-# 02/11/2023: run DIU in all datasets
-
+# 30/01/2024: perform DIU on protein-coding genes from whole dataset
+# 06/02/2024: repeat but with filtered SQANTI3 classification after removal of mono-exonic transcripts within multiexonic genes
 
 ##-------------------------------------------------------------------------
 
@@ -33,7 +32,7 @@ mkdir -p $DIUDIR/targeted $DIUDIR/whole
 mkdir -p $DIUDIR/targeted/sex $DIUDIR/targeted/group $DIUDIR/whole/sex $DIUDIR/whole/group $DIUDIR/whole/allGroup $DIUDIR/whole/allSex
 
 # classification file and target genes
-SQANTIClassFile=${SQANTIDIR}/WholeTargeted_cleaned_aligned_merged_collapsed_qced_RulesFilter_Whole_2reads2samples_classification_noMonoIntergenic.txt
+SQANTIClassFile=${SQANTIDIR}/WholeTargeted_classification_withoutSziKaysSillyRowNames.txt
 METADIR=/lustre/projects/Research_Project-MRC148213/Rosie/SFARIdevelopmentalgenomics/7_differential/3_DIU/metadata
   
 WholeGroupPhenotype=${METADIR}/WholeGroupPhenotype.csv
@@ -45,9 +44,14 @@ WholeSexFactor=${METADIR}/WholeSexFactors.txt
 ProteinCodingGenes=${METADIR}/WholeProteinCodingGenes.txt
 lncRNAGenes=${METADIR}/WholelncRNAGenes.txt
 
-GeneTypes=($ProteinCodingGenes $lncRNAGenes)
-GeneType=${GeneTypes[${SLURM_ARRAY_TASK_ID}]}
+# remove empty lines 
+#cd ${METADIR}/splitWholeGenes/
+#for file in *; do sed -i '/^$/d' "$file"; done
 
+# setting up SLURM
+WholeGenesLists=($(ls ${METADIR}/splitWholeGenes/*))
+WholeGenesList=${WholeGenesLists[${SLURM_ARRAY_TASK_ID}]}
+#echo "${#WholeGenesList[@]}"
 
 ##-------------------------------------------------------------------------
 
@@ -56,7 +60,7 @@ source activate edgeR
 while read gene; do
   echo "$gene"
   # Whole group
-  python ${LOGEN_ROOT}/differential_analysis/subset_and_run_DIU.py ${SQANTIClassFile} ${DESEQDIR}/output_norm_whole_group_removinglateprenatal_TEST.csv -d ${DIUDIR}/whole/allGroup -I ${gene} -p ${WholeGroupPhenotype} -f ${WholeGroupFactor} -n "Whole"
+  python ${LOGEN_ROOT}/differential_analysis/subset_and_run_DIU.py ${SQANTIClassFile} ${DESEQDIR}/DESeq2_whole_transcript_development_normAll.csv -d ${DIUDIR}/whole/allGroup -I ${gene} -p ${WholeGroupPhenotype} -f ${WholeGroupFactor} -n "Whole"
   # Whole sex
-  python ${LOGEN_ROOT}/differential_analysis/subset_and_run_DIU.py ${SQANTIClassFile} ${DESEQDIR}/output_norm_whole_sex_removinglateprenatal_TEST.csv -d ${DIUDIR}/whole/allSex -I ${gene} -p ${WholeSexPhenotype} -f ${WholeSexFactor} -n "Whole"
-done <${GeneType}
+  python ${LOGEN_ROOT}/differential_analysis/subset_and_run_DIU.py ${SQANTIClassFile} ${DESEQDIR}/DESeq2_whole_transcript_sex_normAll.csv -d ${DIUDIR}/whole/allSex -I ${gene} -p ${WholeSexPhenotype} -f ${WholeSexFactor} -n "Whole"
+done < ${WholeGenesList}
