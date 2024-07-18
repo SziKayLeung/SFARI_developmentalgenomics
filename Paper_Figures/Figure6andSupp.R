4f2523d07e04aab593631def4ed6693fed2a84b5
sl693 <sl693@exeter.ac.uk>
2024-07-18 11:58:38 +0100

    supplementary figures

--- Paper_Figures/Figure6andSupp.R
+++ Paper_Figures/Figure6andSupp.R
@@ -3,9 +3,15 @@ monoAllelicDDP = c('ADNP','ANK2','ANKRD11','ARID1B','ASH1L','AUTS2','BCL11A','BC
 SFARI = c('ADNP','AGAP1','ANK2','ANKRD11','ARID1B','ASH1L','ASPM','AUTS2','BCL11A','CACNA1C','CACNA1G','CACNA1H','CADPS','CADPS2','CD38','CDH13','CDH2','CDK13','CELF6','CHD2','CHD8','CLTCL1','CNOT1','CNTN4','CNTN6','CNTNAP2','CSMD1','CTNNA2','CYFIP1','DAGLA','DCC','DISC1','DLG4','DLGAP2','DMD','DPYD','DRD2','DRD3','DYRK1A','ELP4','EP300','FBXO40','FMR1','FOXP1','FOXP2','GABBR2','GPC6','GRIA3','GRIK2','GRIK3','GRIN1','GRIN2A','GRIN2B','H1-4','HERC1','IL1RAPL1','IMMP2L','ITPR1','KAT6B','KATNAL2','KCTD13','KDM6A','KDM6B','KIRREL3','LRBA','MAPT','MCM4','MCPH1','MECP2','MED13L','MEIS2','MET','NEGR1','NF1','NFIA','NFIB','NKX2-2','NLGN1','NLGN2','NLGN3','NLGN4X','NR3C2','NRXN1','NTNG1','NXPH1','OXTR','PARD3B','PAX6','PBX1','PCDH10','PCDH9','PHB','PJA1','POGZ','PRKN','PTEN','PTPRT','QRICH1','RBFOX1','RELN','RPL10','RUNX1T1','SCN1A','SCN2A','SCN8A','SET','SETD1A','SEZ6L2','SHANK3','SLC4A10','SLC9A9','SON','SRRM2','STAG1','STXBP1','SYNGAP1','SYP','TBL1XR1','TBR1','TCF4','TRIO','TSC1','TSC2','TSHZ3','UBE3A','USP7','VPS13B','WWOX','ZBTB16','ZBTB20','ZMYM2','ZNF18','ZNF804A')
 
 schemaGenes <- read.table("/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/0_metadata/schema_genes.txt", col.names = c("Gene"))
+GWAS = c("ACTR1B", "ATP2A2", "BCL11B", "BCL2L12", "BNIP3L", "C12orf43", "CACNA1C", "CALN1", "CISD2", "CLCN3", "CNTN4", "CSMD1", "CTD-2008L17.2", "CUL9", "DCC", "DLGAP2", "DPYD", "EMX1", "ENOX1", "EPN2", "EYS", "FURIN", "GABBR2", "GPM6A", "GPR98", "GRAMD1B", "GRIN2A", "GRM1", "IL1RAPL1", "IMMP2L", "IRF3", "KIAA1549", "KLF6", "LINC00320", "LINC01088", "LRRC4B", "MAD1L1", "MAN2A1", "MAPT", "MSI2", "NAB2", "NEBL", "NEGR1", "NLGN4X", "NRIP1", "NXPH1", "OPCML", "PAK6", "PCGF3", "PCNXL3", "PDE4B", "PJA1", "PLCH2", "PTPRD", "R3HDM2", "RP11-399D6.2", "RP11-507B12.2", "SGCD", "SLC39A8", "SLC4A10", "SNAP91", "SP4", "THAP8", "TMTC1", "TRPC4", "TSNARE1", "TXNRD1", "WSCD2", "ZNF804A", "ZNF823", "ZNF835")
 
+biallelicDDP = c('ASPM','CACNA1G','CHL1','CISD2','CLCN3','CNTNAP2','CTNNA2','DCC','EIF2AK3','EOMES','GLIS3','GPC6','GRIK2','GRIN1','GRIN2A','GRM1','HADH','HERC1','HPSE2','ITPR1','KDM6B','KIAA1109','LMNA','LRBA','MCPH1','NRXN1','NTNG1','ONECUT1','PDIA6','PDX1','PEX16','PPP1R15B','PTF1A','QARS1','RELN','RFX6','SLC39A8','SLF2','TRMT10A','TSPEAR','UBE3B','VPS13B','WFS1','WWOX','ZBTB16','ZFP57')
+
+length(unique(as.character(monoAllelicDDP))) + length(unique(as.character(biallelicDDP)))
 monoAllelicDDPUnique <- setdiff(monoAllelicDDP, SFARI)
 schemaGenesAlone <- setdiff(setdiff(schemaGenes$Gene,SFARI),monoAllelicDDP)
+
+length(unique(c(monoAllelicDDP, biallelicDDP, schemaGenes$Gene, SFARI)))
 
 
 tabulateIF <- function(classf, countcol){
@@ -187,3 +193,66 @@ dev.off()
 pdf("DAGLA.pdf", width = 10, height = 6)
 DAGLAvis
 dev.off()
+
+####
+DIUSig$wholeAllAge <- DIUSig$wholeAllAge %>% mutate(DTE_Dev = ifelse(Gene %in% WholeDTE$age$associated_gene, TRUE, FALSE),
+                                                    IsoformSwitch = ifelse(podiumChange == TRUE, TRUE, FALSE))
+DIUSig$wholeAllAge <- DIUSig$wholeAllAge %>% filter(!is.na(Gene))
+
+p <- venn.diagram(
+  x = list(intersect(unique(WholeDTE$age$associated_gene), ProteinCodingGenes), 
+           intersect(unique(WholeDESeqGeneSig$age$associated_gene), ProteinCodingGenes), 
+           intersect(DIUSig$wholeAllAge$Gene, ProteinCodingGenes),
+           intersect(DIUSig$wholeAllAge[DIUSig$wholeAllAge$podiumChange == TRUE, "Gene"], ProteinCodingGenes)),
+  fill = c("red", "blue","green", "light green"),
+  category.names = c("DTE" , "DGE" , "DIU", "Isoform Switch"),
+  print.mode = "raw", filename = NULL, output = TRUE
+)
+plot_grid(p)
+
+
+## final transcript FICLE
+DIUSig$wholeAllAge <- DIUSig$wholeAllAge %>% mutate(DTE_Dev = ifelse(Gene %in% WholeDTE$age$associated_gene, TRUE, FALSE),
+                                                    IsoformSwitch = ifelse(podiumChange == TRUE, TRUE, FALSE))
+DIUSig$wholeAllAge <- DIUSig$wholeAllAge %>% filter(!is.na(Gene))
+
+p <- venn.diagram(
+  x = list(intersect(unique(WholeDTE$age$associated_gene), ProteinCodingGenes), 
+           intersect(unique(WholeDESeqGeneSig$age$associated_gene), ProteinCodingGenes), 
+           intersect(DIUSig$wholeAllAge$Gene, ProteinCodingGenes),
+           intersect(DIUSig$wholeAllAge[DIUSig$wholeAllAge$podiumChange == TRUE, "Gene"], ProteinCodingGenes)),
+  fill = c("red", "blue","green", "light green"),
+  category.names = c("DTE" , "DGE" , "DIU", "Isoform Switch"),
+  print.mode = "raw", filename = NULL, output = TRUE
+)
+plot_grid(p)
+
+
+finalTranscriptClassification <- distinct(fread(paste0(dirnames$ficle,"/all_final_transcript_classifications.csv"), data.table = F))
+finalTranscriptClassification <- finalTranscriptClassification %>% filter(isoform %in% class.files$glob_SQ$isoform)
+finalTranscriptClassification <- merge(finalTranscriptClassification, class.files$glob_SQ[,c("isoform", "associated_gene", "associated_transcript","structural_category")], by = "isoform")
+finalTranscriptClassificationGene <- aggregate(. ~ associated_gene, finalTranscriptClassificationTranscript %>% select(-isoform), sum)
+
+
+
+finalTranscriptClassificationGene  %>% filter(AS %in% c("A5A3", "AF", "AT", "ES", "IR", "NE_1st", "NE_Int", "NE_Last")) %>%
+  group_by(AS) %>% tally(Frequency) %>% 
+  ggplot(.,aes(x = reorder(AS, -n), y = n)) + geom_bar(stat = "identity") +
+  mytheme + labs(x = "AS events", y = "Frequency") + facet_grid(~DevStatus)
+
+finalTranscriptClassificationTranscript <- merge(finalTranscriptClassification, class.files$glob_SQ[,c("isoform","DevStatus")], by = "isoform")
+finalTranscriptClassificationGene <- aggregate(. ~ associated_gene + DevStatus, 
+                                               finalTranscriptClassificationTranscript %>% select(-isoform), 
+                                               sum)
+finalTranscriptClassificationGene2 <- reshape2::melt(finalTranscriptClassificationGene, variable.name = "AS", value.name = "Frequency", id = c("associated_gene","DevStatus"))
+
+
+finalTranscriptClassificationGene2  %>% filter(AS %in% c("A5A3", "AF", "AT", "ES", "IR", "NE_1st", "NE_Int", "NE_Last")) %>%
+  mutate(DevStatus = factor(DevStatus, levels = c("prenatal","postnatal","Both"))) %>%
+  group_by(AS, DevStatus) %>% tally(Frequency) %>% 
+  ggplot(.,aes(x = reorder(AS, -n), y = n, fill = DevStatus)) + geom_bar(stat = "identity", position = position_dodge()) +
+  mytheme + labs(x = "AS events", y = "Frequency") %>% 
+  scale_fill_manual(labels = c("Prenatal","Postnatal","Both"), name = "", values = c(wes_palette("Royal1")[2],wes_palette("Royal1")[1],wes_palette("Royal1")[4])) +
+  labs(x = "AS events", y = "Frequency") +
+  theme(legend.position = "top")
+
