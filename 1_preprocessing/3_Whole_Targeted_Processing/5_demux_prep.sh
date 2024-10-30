7a2574ef0b241eebed2a3ad36230d7678643bdfe
sl693 <sl693@exeter.ac.uk>
2024-10-30 09:56:18 +0000

    modify demux_prep.sh to generate files from pbmm2 directory rather than tclean (as missing files from gpf shutdown to convert bam files from pbmm2 directory to fasta

--- 1_preprocessing/3_Whole_Targeted_Processing/5_demux_prep.sh
+++ 1_preprocessing/3_Whole_Targeted_Processing/5_demux_prep.sh
@@ -18,28 +18,32 @@ ISOSEQ_COLLAPSE_DIR=/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTR
 TCLEAN_TARGETED_DIR=/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/B_Targeted/4_tclean
 TCLEAN_WHOLE_DIR=/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/A_Whole/4_transcriptClean/Whole
 DEMUX_DIR=/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/C_Whole_Targeted/8_demux
+ALIGNED_WHOLE_DIR=/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/A_Whole/5_isoseq/pbmm2_align
+ALIGNED_TARGETED_DIR=/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/B_Targeted/5_cupcake/5_align_filter
 
 export PATH=$PATH:/lustre/projects/Research_Project-MRC148213/lsl693/scripts/LOGen/assist_ont_processing
 
+mkdir -p ${DEMUX_DIR}/1_ID
 
 ##-------------------------------------------------------------------------
 
+module load Miniconda2 
+
+source activate nanopore 
+
+for i in ${ALIGNED_WHOLE_DIR}/*filtered_sorted.bam; do 
+  echo $i
+  filename=$(basename $i _mapped_filtered_sorted.bam)
+  samtools bam2fq $i | seqtk seq -A - > ${ALIGNED_WHOLE_DIR}/${filename}_mapped_filtered_sorted.fa
+done 
+
+for i in ${ALIGNED_TARGETED_DIR}/*filtered_sorted.bam; do 
+  echo $i
+  filename=$(basename $i _mapped_filtered_sorted.bam)
+  samtools bam2fq $i | seqtk seq -A - > ${ALIGNED_TARGETED_DIR}/${filename}_mapped_filtered_sorted.fa
+done 
+
+
 # generate sample_id.csv for targeted and whole dataset
-adapt_cupcake_to_ont.py ${TCLEAN_TARGETED_DIR} -o Targeted -i clean.fa -d ${DEMUX_DIR}
-adapt_cupcake_to_ont.py ${TCLEAN_WHOLE_DIR} -o Whole -i fa -d ${DEMUX_DIR}
-
-# remoe "combined_sorted" string in Targeted_sample_id.csv (due to the input of the fasta files)
-cd ${DEMUX_DIR}
-awk -F',' '{gsub("_combined_sorted", "", $2); print $1 "," $2}' Targeted_sample_id.csv > Targeted_sample_id_mod.csv
-
-# concatenate whole and targeted sample_id.csv for downstream demux
-cat Whole_sample_id.csv Targeted_sample_id_mod.csv  > WholeTargeted_sample_id.csv
-
-# split read stat per chromosome further into 10 parts
-# i.e.  ${chrNum}.read_stat.renamed > ${chrNum}.read_stat.renamed_part_00
-mkdir -p ${ISOSEQ_COLLAPSE_DIR}/split
-chromosomes=($(printf "chr%s " $(seq 1 22) X Y))
-for chr in ${chromosomes[@]}; do
-  echo $chr
-  split -n l/10 -d ${ISOSEQ_COLLAPSE_DIR}/${chrNum}.read_stat.renamed.txt ${ISOSEQ_COLLAPSE_DIR}/split/${chrNum}_part_
-done
+adapt_cupcake_to_ont.py ${ALIGNED_TARGETED_DIR} -o Targeted -i mapped_filtered_sorted.fa -d ${DEMUX_DIR}/1_ID
+adapt_cupcake_to_ont.py ${ALIGNED_WHOLE_DIR} -o Whole -i mapped_filtered_sorted.fa -d ${DEMUX_DIR}/1_ID
