4f091a0831df72e7fbac1b3d9570574f0735764c
SziKayLeung <szikayleung@gmail.com>
2025-01-09 17:33:52 +0000

    Update differentially protein expression

--- 2_characterisation/2_proteinLevel/8_protein_deseq2.R
+++ 2_characterisation/2_proteinLevel/8_protein_deseq2.R
@@ -1,13 +1,12 @@
 ## ---------- Script -----------------
 ##
-## Purpose: perform differential analysis on AD-BDR Iso-Seq targeted datasets using linear regression
+## Purpose: perform differential analysis on peptides predicted from CPAT
 ## Transcript level separate analysis
 ##
 ## Author: Szi Kay Leung (S.K.Leung@exeter.ac.uk)
 ## https://hbctraining.github.io/DGE_workshop/lessons/04_DGE_DESeq2_analysis.html
 ##
 ## ---------- Notes ------------------
-## two ONT batches: Batch 1 - Nov 2022, Batch 2 - March 2023 
 
 
 ## ---------- packages -----------------
@@ -25,42 +24,32 @@ suppressMessages(library("RColorBrewer"))
 
 ## ---------- source functions -----------------
 
-LOGEN_ROOT = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/scripts/LOGen/"
-source(paste0(LOGEN_ROOT, "/transcriptome_stats/read_sq_classification.R"))
-source(paste0(LOGEN_ROOT, "differential_analysis/run_DESeq2.R"))
-source(paste0(LOGEN_ROOT, "differential_analysis/plot_transcript_level.R"))
-source(paste0(LOGEN_ROOT, "aesthetics_basics_plots/pthemes.R"))
+LOGEN <- "C:/Users/sl693/Dropbox/Scripts/LOGen/"
+source(paste0(LOGEN, "/transcriptome_stats/read_sq_classification.R"))
+source(paste0(LOGEN, "differential_analysis/run_DESeq2.R"))
+source(paste0(LOGEN, "differential_analysis/plot_transcript_level.R"))
+source(paste0(LOGEN, "aesthetics_basics_plots/pthemes.R"))
 
-label_group <- function(genotype){
-  if(genotype %in% c("Case","CASE")){group = "AD"}else{
-    if(genotype %in% c("Control","CONTROL")){group = "Control"}}
-  return(group)
-}
 
 ## ---------- input -----------------
 
 # directory names
-SC_ROOT <- "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/scripts/SFARI_developmentalgenomics/1_characterisation"
-root_dir <- "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/"
-dirnames <- list(
-  sqanti = "/gpfs/mrc0/projects/Research_Project-MRC148213/Rosie/WholeTargeted/SQANTI/",
-  proteomics = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/RBFetal/6a_longReadProteogenomics/",
-  output = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/scripts/SFARI_developmentalgenomics"
-)
-
+root_dir <- "C:/Users/sl693/Dropbox/Scripts/SFARI_developmentalgenomics/data/"
+output_dir <- "C:/Users/sl693/Dropbox/Scripts/SFARI_developmentalgenomics/output/"
 
 # classfiles
-class.files = SQANTI_class_preparation(paste0(dirnames$sqanti,"WholeTargeted_cleaned_aligned_merged_collapsed_qced_RulesFilter_Whole_2reads2samples_classification_noMonoIntergenic_modFL.txt"),"ns")
+load(file = paste0(root_dir,"sqanti/sqantifiltered_monoexonicfiltered_2reads2samples.RData"))
 
 # proteomics input 
-load(file = paste0(SC_ROOT,"/proteinInputWhole.RData"))
+load(file = paste0(root_dir,"proteomics/proteinInputWhole.RData"))
 
 input <- list()
 # phenotype
-input$wholePhenotype <- fread(paste0(root_dir, "RBFetal/00_metadata/WholeTargetedphenotype_fixedsex.csv"),data.table=F, stringsAsFactors=F) %>% 
+input$wholePhenotype <- fread(paste0(root_dir, "metadata/WholeTargetedphenotype_fixedsex.csv"),data.table=F, stringsAsFactors=F) %>% 
   mutate(group = factor(group, levels = c("Prenatal","Postnatal")), col = paste0(sample,"_",group)) %>% 
   filter(grepl("Whole",sample))
-input$classfiles <- class.files %>% select(isoform, associated_gene, associated_transcript, structural_category, subcategory, contains("Whole"))
+input$classfiles <- class.files$glob_SQ %>% select(isoform, associated_gene, associated_transcript, structural_category, subcategory, 
+                                                   contains("Whole", ignore.case = FALSE))
 row.names(input$classfiles) <- input$classfiles$isoform
 
 
@@ -72,8 +61,8 @@ phenoFiles <- list(
 
 ### protein 
 # aggregate sum by same peptide sequence
-counts <- class.files %>% select(isoform, contains("Whole"))
-pFL <- proteinInput$t.class.files %>% dplyr::select(base_acc, contains("Whole")) 
+counts <- class.files$glob_SQ %>% select(isoform, contains("Whole", ignore.case = FALSE))
+pFL <- proteinInput$t.class.files %>% dplyr::select(base_acc, contains("Whole", ignore.case = FALSE)) 
 pFLsum <- aggregate(. ~ base_acc, pFL, sum)
 
 # datawrangle for input to run_DESeq2()
@@ -87,16 +76,12 @@ row.names(expressionFiles$iso) <- input$classfiles$isoform
 ## ---------- ONT: Creating DESeq2 object and analysis -----------------
 
 ResTran <- list(
-  #tWald = run_DESeq2(test="Wald",expressionFiles$iso,phenoFiles$isoGrp,threshold=10,controlname="Control",design="case_control",groupvar="factor"),
   pWald = run_DESeq2(test="Wald",expressionFiles$isoProtein,phenoFiles$wholeGroup,threshold=10,controlname="Prenatal",design="case_control",groupvar="factor")
 )
 
-annoResTran <- list(
-  #Wald = anno_DESeq2(ResTran$tWald,input$classfiles,phenoFiles$isoGrp,controlname="Control",level="transcript",sig=0.05),
-  pWald = anno_DESeq2(ResTran$pWald,input$classfiles,phenoFiles$wholeGroup,controlname="Prenatal",level="transcript",sig=0.05)
-)
-
+ResTran$pWald$annoRes <- merge(ResTran$pWald$res_Wald[ResTran$pWald$res_Wald$padj < 0.05, ],
+      input$classfiles[,c("isoform","associated_gene","associated_transcript","structural_category","subcategory")], by = "isoform")
 
 ## ---------- Output -----------------
 
-saveRDS(annoResTran, file = paste0(dirnames$output, "/IsoProtein_Whole_DESeq2TranscriptLevel.RDS"))
+saveRDS(ResTran, file = paste0(output_dir, "/IsoProtein_Whole_DESeq2TranscriptLevel.RDS"))
