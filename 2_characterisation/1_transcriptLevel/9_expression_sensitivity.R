67b4f5a1fb047e963e420c7b48fe8a4abe461f9b
SziKayLeung <33493350+SziKayLeung@users.noreply.github.com>
2025-05-12 09:57:44 +0100

    Update 9_expression_sensitivity.R
    logscale sensitivity curve

--- 2_characterisation/1_transcriptLevel/9_expression_sensitivity.R
+++ 2_characterisation/1_transcriptLevel/9_expression_sensitivity.R
@@ -29,7 +29,9 @@ dat2 <- sqanti %>%
   mutate(cate_sumFL = factor(cate_sumFL, levels = c(seq(1:9),"10","10-20","21-30","31-40","41-50","> 50")))
 ggplot(dat2, aes(x = as.factor(whole_nsamples), y = proportion, colour = cate_sumFL, group = cate_sumFL)) +
   geom_point() +
-  geom_line() + scale_y_continuous(labels = perc_lab)  + 
+  geom_line() + scale_y_log10(
+    labels = percent_format(accuracy = 1)  # keeps labels as percentages
+  ) + 
   labs(x = "Minimum number of samples", y = "Reads (%)",
        colour = "Minimum number of reads") +
   theme_classic() 
@@ -47,3 +49,6 @@ diff %>% group_by(structural_category, subcategory, monomulti) %>% tally() %>%
 ggplot(., aes(x = structural_category, y = n, fill = subcategory)) + 
 	geom_bar(stat = "identity") + facet_grid(~monomulti) +
 	labs(y = "number of transcripts removed")
+
+# output
+write.table(as.data.frame(dat2), "/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/SFARI/C_Whole_Targeted/9_sqanti_final/sensitivity_expression_threshold.txt", sep = "\t", quote = F)
